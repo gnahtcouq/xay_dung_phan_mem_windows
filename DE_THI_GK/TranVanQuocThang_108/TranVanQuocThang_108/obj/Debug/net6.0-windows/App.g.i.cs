30ca76a8cb76db737045bcbb826922a48c658adf
Quoc Thang <comehere.thang@gmail.com>
2024-03-20 10:35:26 +0700

    /
    /

--- DE_THI_GK/TranVanQuocThang_108/TranVanQuocThang_108/obj/Debug/net6.0-windows/App.g.i.cs
+++ DE_THI_GK/TranVanQuocThang_108/TranVanQuocThang_108/obj/Debug/net6.0-windows/App.g.i.cs
@@ -60,7 +60,7 @@ namespace TranVanQuocThang_108 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/TranVanQuocThang_108;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TranVanQuocThang_108;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
